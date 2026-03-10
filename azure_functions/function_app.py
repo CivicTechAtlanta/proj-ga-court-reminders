000fa13f193dc7b378f3a64c1879822e6d573046
Yaw Agyepong <yaw.agyepong@gmail.com>
2026-03-09 20:42:39 -0400

    printing out json body function receives

--- azure_functions/function_app.py
+++ azure_functions/function_app.py
@@ -12,21 +12,7 @@ def twilioHandler(req: func.HttpRequest) -> func.HttpResponse:
         "court-reminder %s: HTTP trigger function processed a request.", __version__
     )
 
-    name = req.params.get("name")
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get("name")
+    body = req.get_json()
+    logging.info("received body: %s", body)
 
-    if name:
-        return func.HttpResponse(
-            f"Hello, {name}. This HTTP triggered function executed successfully."
-        )
-    else:
-        return func.HttpResponse(
-            "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-            status_code=200,
-        )
+    return func.HttpResponse("done\n", status_code=200)
